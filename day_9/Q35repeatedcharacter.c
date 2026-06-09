2c7d86f5749a31639eaf440d1af0f4be92233a64
MmayankYyadav <yadavm26695@mail.com>
2026-06-09 13:15:35 +0530

    day_9 assignment is uploaded

---
+++ day_9/Q35repeatedcharacter.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+int number,x=0;
+printf("enter number = ");
+scanf("%d", &number);
+for(int i=65;i<=64+number;i++){
+for(int j=65 ;j<=i;j++){
+    printf("%c",i);}
+printf("\n");
+}
+return 0;}
