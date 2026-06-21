cdb261458b594dbb4f5c1862afba96326d07a2f6
MmayankYyadav <yadavm26695@mail.com>
2026-06-21 21:26:43 +0530

    day_21 assignment is uploaded

--- day_21/Q82reversestring.cpp
+++ day_21/Q82reversestring.cpp
@@ -5,7 +5,7 @@ string str;
 cout << "enter you string to reverse it = ";
 cin >> str ;
 int count = 0;
-cout << "reversed string is"
+cout << "reversed string is";
 for( int i = 0 ; i <= str.length(); i++){
     cout << str[str.length() - i];
 }
