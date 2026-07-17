af951251283d98e78062381d5cafdea2caab6cd5
MmayankYyadav <yadavm26695@gmail.com>
2026-07-17 11:39:32 +0530

    just a demo

---
+++ demo/hello.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h> 
+using namespace std; 
+
+int main (){ 
+    string name; 
+    cout << "enter you name : " ; 
+    
+    cin >> name; 
+    cout << "hello " << name <<" !!" << endl; 
+    return 0; 
+}
